22bc17de9f6d103bd904a44581185f38ee13ba14
i-xi-dev <naoyuki.isono@gmail.com>
2023-07-22 20:44:28 +0900

    3.0.10

--- deps.ts
+++ deps.ts
@@ -2,4 +2,4 @@ export {
   AbortError,
   InvalidStateError,
 } from "https://raw.githubusercontent.com/i-xi-dev/exception.es/1.0.5/mod.ts";
-export { Reading } from "https://raw.githubusercontent.com/i-xi-dev/reading.es/1.0.9/mod.ts";
+export { Reading } from "https://raw.githubusercontent.com/i-xi-dev/reading.es/1.0.10/mod.ts";
