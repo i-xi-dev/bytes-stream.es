725269cbcf05e482cead15cc3aa80aea8c0ce4eb
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-07 00:44:38 +0900

    3.0.22

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@ export {
   AbortError,
   InvalidStateError,
 } from "https://raw.githubusercontent.com/i-xi-dev/exception.es/1.1.2/mod.ts";
-export { GrowableBuffer } from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/3.2.2/mod.ts";
-export { Reading } from "https://raw.githubusercontent.com/i-xi-dev/reading.es/1.0.20/mod.ts";
+export { GrowableBuffer } from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/4.0.6/mod.ts";
+export { Reading } from "https://raw.githubusercontent.com/i-xi-dev/reading.es/1.0.21/mod.ts";
