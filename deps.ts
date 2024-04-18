773a0dd25e32275edbbd9dac9b9cc8b0c5fb171d
i-xi-dev <naoyuki.isono@gmail.com>
2024-04-19 00:22:54 +0900

    3.0.30

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@ export {
   AbortError,
   InvalidStateError,
 } from "https://raw.githubusercontent.com/i-xi-dev/exception.es/1.1.4/mod.ts";
-export { GrowableBuffer } from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/6.2.2/mod.ts";
-export { Loading } from "https://raw.githubusercontent.com/i-xi-dev/loading.es/2.0.1/mod.ts";
+export { GrowableBuffer } from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/6.2.3/mod.ts";
+export { Loading } from "https://raw.githubusercontent.com/i-xi-dev/loading.es/2.0.2/mod.ts";
--- tests/deps.ts
+++ tests/deps.ts
@@ -1,4 +1,4 @@
 export {
   assertNotStrictEquals,
   assertStrictEquals,
-} from "https://deno.land/std@0.215.0/assert/mod.ts";
+} from "https://deno.land/std@0.223.0/assert/mod.ts";
